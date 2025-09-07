d0149aaa84aaee1a58cf4e4702ac2a2d3dbfaabd
jeetu <jitendradew2003@gmail.com>
2025-09-07 17:50:00 +0530

    prep for deploy

--- src/server.ts
+++ src/server.ts
@@ -14,8 +14,14 @@ const PORT = process.env.PORT || 8000;
 
 connectDB();
 
+const allowedOrigins = ["http://localhost:3000"];
 //Middleware
-app.use(cors());
+app.use(
+  cors({
+    origin: allowedOrigins,
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
